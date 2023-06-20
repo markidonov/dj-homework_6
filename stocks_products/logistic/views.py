ece02ccf3aaf5411591a683d8a7e7e790071fff3
markidonov <markidonov.vm@cyberform.systems>
2023-06-21 00:59:57 +0700

    initial

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
+    pagination_class = PageNumberPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    pagination_class = PageNumberPagination
+    filterset_fields = ['products']
